9f8e6f075cbc4eb63c8ff0722d047d2cd21c2019
lentilus <lentilus@mailbox.org>
2024-04-13 00:23:37 +0200

    implemented zettel insert, rm etc

--- lua/telescope/_extensions/xettelkasten.lua
+++ lua/telescope/_extensions/xettelkasten.lua
@@ -2,7 +2,9 @@
 return require("telescope").register_extension {
   setup = function(ext_config, config) end,
   exports = {
-    zettel = require("xettelkasten").zettel,
-    ref = require("xettelkasten").ref,
+    ref_rm = require("xettelkasten").ref_rm,
+    ref_find = require("xettelkasten").ref_find,
+    ref_insert = require("xettelkasten").ref_insert,
+    zettel_find = require("xettelkasten").zettel_find,
   },
 }
